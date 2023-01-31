b54abd8371cfaf97ce5cf4a56197255fd7de8a7f
happy.sharma <happykumar.sharma@nextgenclearing.com>
2023-01-31 22:57:40 +0530

    chat improvision - dynamic questions

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -2,23 +2,23 @@ let text = "Clam Offer Now";
 let offer = text.link("https://businessara.tech");
 
 function getBotResponse(input) {
-    //rock paper scissors
-    if (input == "rock") {
-        return "paper";
-    } else if (input == "paper") {
-        return "scissors";
-    } else if (input == "scissors") {
-        return "rock";
-    }
+  //rock paper scissors
+  if (input == "rock") {
+    return "paper";
+  } else if (input == "paper") {
+    return "scissors";
+  } else if (input == "scissors") {
+    return "rock";
+  }
 
-    // Simple responses
-    if (input == "hello") {
-        return "Hello there!";
-    } else if (input == "goodbye") {
-        return "Talk to you later!";
-    } else if (input == "I Love BusinessAra"){
-        return "Thanks, We would love to give you a very special offer " + offer
-    } else {
-        return "Talk with a live expert now"+ result;
-    }
+  // Simple responses
+  if (input == "hello") {
+    return "Hello there!";
+  } else if (input == "goodbye") {
+    return "Talk to you later!";
+  } else if (input == "I Love BusinessAra") {
+    return "Thanks, We would love to give you a very special offer " + offer;
+  } else {
+    return "Talk with a live expert now" + result;
+  }
 }
